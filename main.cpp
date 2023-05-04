cf06ca2a9d689af28a1d44e1ecb8505c750315e0
alouane04 <ariahi@student.1337.ma>
2023-05-04 13:16:45 +0100

    add func of parse confi

--- main.cpp
+++ main.cpp
@@ -10,7 +10,7 @@ using namespace std;
 ifstream& openFileStream(int ac ,char **av)
 {
     if (ac > 2)
-        throw invalid_argument("arguments  > 2");
+        throw invalid_argument("arguments > 2");
     else if (ac == 2)
     {
         ifstream conf_file(static_cast<string>(av[1]));
@@ -30,22 +30,45 @@ ifstream& openFileStream(int ac ,char **av)
 int	checkConfigFile(int ac, char **av, ConfigFile& config)
 {
     ifstream &conf_file = openFileStream(ac ,av);
+	// here we gonna make the parse gerer bzaf dyal servers
 	parseConfig(conf_file, config);
 }
 
-string parseConfig(ifstream& conf_file, ConfigFile& config)
+void	test(string &test, string code)
+{}
+
+/**
+ * The function parses a configuration file and extracts information about server and location
+ * settings.
+ */
+ifstream& parseConfig(ifstream& conf_file, ConfigFile& config)
 {
     string line;
 
+	if (!getline(conf_file, line, '{') || line.substr(0, 7) != "server " || line.size() != 7)
+		throw invalid_argument("config file syntax error");
+
     while (getline(conf_file, line, '\n'))
     {
-
+		if (line.substr(0, 10) == "location {")
+		{
+			if (line.size() != 10)
+				throw invalid_argument("config file syntax error");
+			while (getline(conf_file, line, '\n'))
+			{
+				if (line.find("}") != string::npos)
+					break;
+				test(line, "location");
+			}
+			continue;
+		}
+		test(line, "server");
     }
 }
 
 int main(int ac, char **av)
 {
-    ConfigFile  config();
+    ConfigFile  config;
     checkConfigFile(ac, av, config);
 
     return (0);
